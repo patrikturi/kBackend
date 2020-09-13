149ce0939217fd2f0208a12ab0cb5d100b98241b
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-09-13 17:56:18 +0200

    refactor: rename kbackend to core

--- kbackend/core/settings.py
+++ kbackend/core/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'kbackend.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'kbackend.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
