48295d75cd9f365cc382a30ca8510c97d1a47412
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-10-20 22:36:53 +0200

    chore: hide gunicorn version

--- kbackend/core/wsgi.py
+++ kbackend/core/wsgi.py
@@ -10,9 +10,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import gunicorn
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
+
+gunicorn.SERVER_SOFTWARE = 'undisclosed'
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
